8db90f84144f7ec4dc0335b32b72366289d8077d
quyhp <tuyhpq@gmail.com>
2018-04-24 00:30:30 +0700

    ...

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 module.exports = {
     'BASE_URL': 'https://vipfb.ru',
-    'ID': '100007665604062',
+    'ID': '100003890791233',
     'ACCESS_TOKEN': [
-        'EAAAAUaZA8jlABAAexyMP0jDe88wzskp3JvIIGr3Or4ZAlEzmoZAs1SpDVRZAtJH2TtNwfBBYVOebZAEtz5gqx65aoTtTNrQilpBY2z8JSxv4k0gkuSPnKDHN72UlZAqmwfgCNZB3LNX3gmTu0flJgZCaCPhF2kOmsjZAWfM6dENpjZBrgDuEsgZCBPIsFFUdwylVlMZD',
-        'EAAAAUaZA8jlABALnf8f44aclMTy6nUDkHzZB0YGNGSm3Oc4ZAjfByobluaUP3dRCgZC7moBtFruWuBplUq42NoHRarmUqvbYchRW9bQZC76vciydwZANdnKzxltwevKz1NxOpLaMWxlLNUyvTUtmh8skMujnGqgSmMsaTJGVpYeAqTMTZCgaSqCAvGDZCXZASXloZD'
+        'EAAAAUaZA8jlABAEtEUwx3zV3ZALRdkXGamfmzyyj1PikmtLKGjuuIWiJXZBTExuwsLVm3yjd8PjPDGTXqyqZBb6fJ6aNZCvvLaJUxSHW8ZBuTugjuH8faRLozHL97c5z24UJ3RsC3SRTCTpCqTElv7feTRBzyAydjJZCv0YjrUlXy19wcclcfTr8GFewi7iA3sZD'
     ],
     'OCR_KEY': '9bdf65ced588957'
 }
